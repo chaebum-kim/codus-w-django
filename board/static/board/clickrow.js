4583c57287a029b789028aaf8179e0a0712d545d
Chaebum Kim <tw2cha@gmail.com>
2021-06-15 19:14:08 +0900

    Revert "Add user page"
    This reverts commit 1159f1b0f08cabcd7fd32b998b495925f14b418a.

--- board/static/board/clickrow.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("tbody").addEventListener("click", (e) => {
-    const target = e.target.parentElement;
-    window.location.href = target.dataset.href;
-  });
-});
